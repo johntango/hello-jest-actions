16a98aaf93785b2d4975595c6e27ee8ca86a8035
John Williams <jrwtango@gmail.com>
2020-09-24 09:01:19 -0400

    test8

--- hello.test.js
+++ hello.test.js
@@ -1,7 +1,5 @@
 const hello = require("./hello");
 
 it("My hello", () => {
-  test("works", () => {
-    expect(hello.hello()).toEqual("Hello World");
-  });
+  expect(hello.hello()).toEqual("Hello World");
 });
